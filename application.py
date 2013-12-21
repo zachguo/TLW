39769b7005a1c681fbbb4702c839c8fc025b053f
Zach Guo <zachguo@gmail.com>
2013-12-21 00:10:56 -0500

    add nBIC/nUIC, better result display

--- application.py
+++ application.py
@@ -10,12 +10,14 @@ app.debug=True # only in dev
 def demo():
 	if request.method == 'POST':
 		tlw = TLW.TLW()
-		wordICunigram, wordICbigram, error = '', '', ''
+		wordICunigram, wordICbigram, wordICunigram_std, wordICbigram_std, error, lexeme, citation = '','','','','','',''
 		if 'lexeme' in request.form:
 			lexeme = escape(request.form['lexeme']).strip()
 			if lexeme:
 				if lexeme.encode('utf-8') in tlw.citationDict:
-					wordICunigram, wordICbigram = tlw.cal_wellformedness(tlw.citationDict[lexeme.encode('utf-8')])
+					citation = tlw.citationDict[lexeme.encode('utf-8')]
+					wordICunigram, wordICbigram = tlw.cal_wellformedness(citation)
+					citation = citation.decode('utf-8')
 				else:
 					error = "Sorry, the lexeme you entered doesn't exist in our corpus. Please try its citation instead."
 		if 'citation' in request.form:
@@ -25,7 +27,11 @@ def demo():
 					wordICunigram, wordICbigram = tlw.cal_wellformedness(citation.encode('utf-8'))
 				except:
 					error = "Invalid citation."
-		return render_template("_base.html", wordICunigram=wordICunigram, wordICbigram=wordICbigram, error=error)
+		if wordICunigram:
+			wordICunigram_std = -(wordICunigram - 4.325948)/0.3538684 # standardized value
+		if wordICbigram:
+			wordICbigram_std = -(wordICbigram - 11.38079)/1.537547 # standardized value
+		return render_template("_base.html", wordICunigram=wordICunigram, wordICbigram=wordICbigram, wordICunigram_std=wordICunigram_std, wordICbigram_std=wordICbigram_std, error=error, lexeme=lexeme, citation=citation)
 	else:
 	    return render_template("_base.html")
 
